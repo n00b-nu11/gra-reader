ba443b5a8751e43592ef48b2d99d03928faf1e31
n00b-nu11 <108191157+n00b-nu11@users.noreply.github.com>
2022-08-21 15:18:06 +0900

    fix: small

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import asyncio
+from xml.dom.minidom import parseString
 import cv2
 import easyocr
 import os
@@ -8,6 +9,8 @@ import pyautogui
 import sys
 import subprocess
 import time
+
+before_message = ""
 
 class GraReader:
     """
@@ -18,7 +21,8 @@ class GraReader:
     def __init__(self,
                  interval_sec: float = 0.5,
                  lang_list: list[str] = ['ja', 'en'],
-                 screenshot_range=(210, 1400, 600, 200)):
+                 # screenshot_range=(left, top, width, height)
+                 screenshot_range=(130, 1400, 630, 200)):
         self.interval_sec = interval_sec
         self.lang_list = lang_list
         self.reader = easyocr.Reader(lang_list)
@@ -29,7 +33,7 @@ class GraReader:
             os.mkdir("./assets/")
         print('終了したいときはCtrl-Cを押してください')
 
-    def run(self):
+    async def run(self):
         while True:
             # fix this(async)
             self.__worker()
@@ -52,24 +56,34 @@ class GraReader:
         past_img = cv2.imread(updated_path)
         if past_img is not None:
             diff = reversed_img.astype(int) - past_img.astype(int)
-            if diff.max() - diff.min() < 100:
+            if diff.max() - diff.min() < 500:
                 return False
 
         cv2.imwrite(updated_path, reversed_img)
         return True
     
     def say(self, text : str):
-        command = ['voicevox-cli', '-speaker=1', '-style=0', text]
+        global before_message
+        try:
+            idx = text.index("が音")
+            text = text[:idx] + "さん、こんにちは！"
+        except:
+            # ただのメッセージのはずなので、そのまま読み上げる
+            pass
+
+        if before_message == text:
+            print('duplicated message', file=sys.stderr)
+            return
+        
+        print(f'read text: {text}')
+        before_message = text
+        command = ['voicevox-cli', '-speaker=4', '-style=0', text]
         cp = subprocess.run(command)
         return cp
 
     def __worker(self):
         # take a screenshot to recognize a text
         pyautogui.screenshot(self.target_path, region=self.screenshot_range)
-
-        # if not changed, do nothing
-        if not self.__state_is_changed(self.target_path, self.updated_path):
-            return
 
         extracted_text = self.__extract_text_from_image(self.updated_path)
         if extracted_text is None:
